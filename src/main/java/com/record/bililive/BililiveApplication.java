1bf2fe81d3b48d6352ab631c86b708c7395e7869
lizhongqu <528605207@qq.com>
2023-09-24 10:40:06 +0800

    Initial commit

---
+++ src/main/java/com/record/bililive/BililiveApplication.java
@@ -0,0 +1,13 @@
+package com.record.bililive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BililiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BililiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/record/bililive/BililiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record.bililive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BililiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
